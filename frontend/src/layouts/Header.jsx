dd98f8572e0c94e889ac7e2bbf154214e37557b8
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2024-10-12 21:17:48 -0400

    fix: endline and header size

--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="flex bg-[#881337] h-16 items-center justify-center w-full">
+    <div className="flex bg-[#881337] h-[100px] items-center justify-center w-full">
       <p className="text-white font-roboto text-[28px] font-bold">
         Nombre Empresa Super cool
       </p>
