7b14b90e58562b18639cea253f15becce4219b2b
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2024-10-13 22:05:56 -0400

    fix: top round and requierenments

--- frontend/src/components/RoomCard.jsx
+++ frontend/src/components/RoomCard.jsx
@@ -4,14 +4,14 @@ import { IoIosArrowDropleftCircle } from "react-icons/io";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 import Button from './Button';
 
-export default function RoomCard({bed, capacity, price, floor, number, services})
+export default function RoomCard({bed, capacity, price, floor, code, services})
 {
   const [details, setDetails] = useState(false);
 
   
   return(
     <div className='flex flex-col font-roboto w-full'>
-      <div className={`flex flex-row justify-between bg-white p-5 rounded${details? '-t': ''}-[20px] border-[#881337] border-2`}>
+      <div className={`flex flex-row justify-between bg-white p-5 ${details? 'rounded-t-[20px]': 'rounded-[20px]'} border-[#881337] border-2`}>
         <div className='flex flex-row justify justify-between'>
           <div className='flex flex-row px-3'>
             <p className='text-[20px] font-bold'>Cama: </p>
@@ -24,7 +24,7 @@ export default function RoomCard({bed, capacity, price, floor, number, services}
         </div>
         <div className='flex flex-row'>
           <p className='px-3 text-[20px] font-bold'>{price}Bs</p>
-          <button onClick={() => setDetails((presed) => !presed)}>
+          <button onClick={() => setDetails((details) => !details)}>
             {details ?  <IoIosArrowDropdownCircle size={30} /> : <IoIosArrowDropleftCircle size={30} /> }
           </button>
         </div>
@@ -37,7 +37,7 @@ export default function RoomCard({bed, capacity, price, floor, number, services}
           </div>
           <div className='flex flex-row px-3 py-2'>
             <p className='text-[20px] font-bold'>Numero de Habitación:</p>
-            <p className='text-[20px] px-1'>{number}</p>
+            <p className='text-[20px] px-1'>{code}</p>
           </div>
         </div>
         <div className='items-center text-center'>
@@ -56,10 +56,10 @@ export default function RoomCard({bed, capacity, price, floor, number, services}
 }
 
 RoomCard.propTypes = {
-  bed: PropTypes.string,
-  capacity: PropTypes.number,
-  price: PropTypes.number,
-  floor: PropTypes.number,
-  number: PropTypes.number,
-  services: PropTypes.array
+  bed: PropTypes.string.isRequired,
+  capacity: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  floor: PropTypes.number.isRequired,
+  code: PropTypes.string.isRequired,
+  services: PropTypes.array.isRequired
 }
