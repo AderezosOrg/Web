b175187c3f5422fe24799bba357c74e8d83f8e46
Hugo Oropeza <oropezahugo35@gmail.com>
2024-10-16 21:54:36 -0400

    feat: add bed, contact, hotel, service, and user DAO

---
+++ backend/DB/Operations/Concrete/HotelDAO.cs
@@ -0,0 +1,161 @@
+using System.Data;
+using System.Text;
+using Entities;
+using MySql.Data.MySqlClient;
+
+namespace Db;
+
+public class HotelDAO : IDAO<Hotel>
+{
+    public int Create(Hotel h)
+    {
+        string IdC = h.HotelID.ToString();
+        string stars = h.Stars.ToString();
+        string name = h.Name;
+        string allowPets = ObjectMapper.MapBoolean(h.AllowsPets);
+        string address = h.Address;
+        string tax = h.Tax.ToString();
+        string userId = h.UserID.ToString();
+        string contactId = h.ContactID.ToString();
+        string bathroomId = h.BathRoomID.ToString();
+
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("INSERT INTO Hotel (Id, Stars, Name, AllowPets, Address, Tax, UserID, ContactId, BathroomId)")
+            .Append("VALUES ('").Append(IdC).Append("',")
+                                .Append(stars).Append(",")
+                                .Append(name).Append(",")
+                                .Append(allowPets).Append(",")
+                                .Append(address).Append(",")
+                                .Append(tax).Append(",")
+                                .Append(userId).Append(",")
+                                .Append(contactId).Append(",")
+                                .Append(bathroomId).Append(");");
+
+        com.CommandText = sb.ToString();
+        return com.ExecuteNonQuery();
+    }
+
+    public Hotel? Read(Guid Id)
+    {
+        string IdC = Id.ToString();
+
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("SELECT * FROM Hotel WHERE Id = '").Append(IdC).Append("';");
+
+        com.CommandText = sb.ToString();
+        var reader = com.ExecuteReader();
+        if (!reader.HasRows) return null;
+        reader.Read();
+        
+        Hotel toReturn = new Hotel() {
+            HotelID = Guid.Parse(reader.GetString(0)),
+            Stars = reader.GetInt16(1),
+            Name = reader.GetString(2),
+            AllowsPets = reader.GetBoolean(3),
+            Address = reader.GetString(4),
+            Tax = reader.GetDecimal(5),
+            UserID = Guid.Parse(reader.GetString(6)),
+            ContactID = Guid.Parse(reader.GetString(7)),
+            BathRoomID = Guid.Parse(reader.GetString(8))
+        };
+        reader.Close();
+        return toReturn;
+    }
+
+    public List<Hotel>? ReadAll()
+    {
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Hotel");
+
+        com.CommandText = sb.ToString();
+        com.CommandType = CommandType.TableDirect;
+
+        Hotel toAppend;
+
+        var reader = com.ExecuteReader();
+        List<Hotel> toReturn = new List<Hotel>();
+        while (reader.Read())
+        {
+            toAppend = new Hotel() {
+                HotelID = Guid.Parse(reader.GetString(0)),
+                Stars = reader.GetInt16(1),
+                Name = reader.GetString(2),
+                AllowsPets = reader.GetBoolean(3),
+                Address = reader.GetString(4),
+                Tax = reader.GetDecimal(5),
+                UserID = Guid.Parse(reader.GetString(6)),
+                ContactID = Guid.Parse(reader.GetString(7)),
+                BathRoomID = Guid.Parse(reader.GetString(8))
+            };
+
+            toReturn.Add(toAppend);
+        }
+        reader.Close();
+        return toReturn;
+    }
+
+    public int Update(Hotel h)
+    {
+        string IdC = h.HotelID.ToString();
+        string stars = h.Stars.ToString();
+        string name = h.Name;
+        string allowPets = ObjectMapper.MapBoolean(h.AllowsPets);
+        string address = h.Address;
+        string tax = h.Tax.ToString();
+        string userId = h.UserID.ToString();
+        string contactId = h.ContactID.ToString();
+        string bathroomId = h.BathRoomID.ToString();
+
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("UPDATE Hotel ")
+            .Append("SET Stars = ").Append(stars).Append(", ")
+            .Append("Name =").Append(name).Append(", ")
+            .Append("AllowPets =").Append(allowPets).Append(", ")
+            .Append("Address =").Append(address).Append(", ")
+            .Append("Tax =").Append(tax).Append(", ")
+            .Append("UserId =").Append(userId).Append(", ")
+            .Append("ContactId =").Append(contactId).Append(", ")
+            .Append("BathRoomId =").Append(bathroomId)
+            .Append(" WHERE Id = '").Append(IdC).Append("';");
+        com.CommandText = sb.ToString();
+        var reader = com.ExecuteReader();
+        int toReturn = reader.RecordsAffected;
+        reader.Close();
+            
+        return toReturn;
+    }
+
+    public bool Delete(Guid Id)
+    {
+        string IdC = Id.ToString();
+
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("DELETE FROM Hotel ")
+            .Append(" WHERE Id = '").Append(IdC).Append("';");
+
+        com.CommandText = sb.ToString();
+        var reader = com.ExecuteReader();
+        int recordsAffected;
+
+        reader.Read();
+        recordsAffected = reader.RecordsAffected;
+        reader.Close();
+
+        return recordsAffected>0;
+    }
+}
---
+++ backend/DB/Operations/Concrete/UserDAO.cs
@@ -0,0 +1,132 @@
+using System.Data;
+using System.Text;
+using Entities;
+using MySql.Data.MySqlClient;
+
+namespace Db;
+
+public class UserDAO : IDAO<User>
+{
+    
+    public int Create(User h)
+    {
+        string IdC = h.UserID.ToString();
+        string name = h.Name;
+        string ciNumber = h.CINumber;
+        string contactId = h.ContactID.ToString();
+
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("INSERT INTO User (Id, Name, CINumber, ContactId)) ")
+            .Append("VALUES ('").Append(IdC).Append("',")
+                                .Append(name).Append(",")
+                                .Append(ciNumber).Append(",")
+                                .Append(contactId).Append(");");
+
+        com.CommandText = sb.ToString();
+        return com.ExecuteNonQuery();
+    }
+
+    public User? Read(Guid Id)
+    {
+        string IdC = Id.ToString();
+
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("SELECT * FROM User WHERE Id = '").Append(IdC).Append("';");
+
+        com.CommandText = sb.ToString();
+        var reader = com.ExecuteReader();
+        if (!reader.HasRows) return null;
+        reader.Read();
+        
+        User toReturn = new User {
+            UserID = Guid.Parse(reader.GetString(0)),
+            Name = reader.GetString(1),
+            CINumber = reader.GetString(2),
+            ContactID = Guid.Parse(reader.GetString(3))
+        };
+        reader.Close();
+        return toReturn;
+    }
+
+    public List<User>? ReadAll()
+    {
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("User");
+
+        com.CommandText = sb.ToString();
+        com.CommandType = CommandType.TableDirect;
+
+        User toAppend;
+
+        var reader = com.ExecuteReader();
+        List<User> toReturn = new List<User>();
+        while (reader.Read())
+        {
+            toAppend = new User() {
+                UserID = Guid.Parse(reader.GetString(0)),
+                Name = reader.GetString(1),
+                CINumber = reader.GetString(2),
+                ContactID = Guid.Parse(reader.GetString(3))
+            };
+
+            toReturn.Add(toAppend);
+        }
+        reader.Close();
+        return toReturn;
+    }
+
+    public int Update(User s)
+    {
+        string IdC = s.UserID.ToString();
+        string name = s.Name;
+        string ciNumber = s.CINumber;
+        string contactId = s.ContactID.ToString();
+
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("UPDATE User ")
+            .Append("SET Name = ").Append(name).Append(", ")
+            .Append("CINumber =").Append(ciNumber).Append(", ")
+            .Append("ContactId =").Append(contactId)
+            .Append(" WHERE Id = '").Append(IdC).Append("';");
+        com.CommandText = sb.ToString();
+        var reader = com.ExecuteReader();
+        int toReturn = reader.RecordsAffected;
+        reader.Close();
+            
+        return toReturn;
+    }
+
+    public bool Delete(Guid Id)
+    {
+        string IdC = Id.ToString();
+
+        MySqlCommand com = new MySqlCommand();
+        com.Connection = DbUtils.GetConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("DELETE FROM User ")
+            .Append(" WHERE Id = '").Append(IdC).Append("';");
+
+        com.CommandText = sb.ToString();
+        var reader = com.ExecuteReader();
+        int recordsAffected;
+
+        reader.Read();
+        recordsAffected = reader.RecordsAffected;
+        reader.Close();
+
+        return recordsAffected>0;
+    }
+}
