f624f9eb90574f66f84d68a0a00244f2fbf3df77
Hugo Oropeza <oropezahugo35@gmail.com>
2024-10-20 16:17:13 -0400

    feat: add login post

---
+++ backend/DTOs/WithoutId/LoginDTO.cs
@@ -0,0 +1,7 @@
+namespace DTOs.WithoutId;
+
+public class LoginDTO
+{
+    public string Email { get; set; }
+    public string Token { get; set; }
+}
---
+++ backend/DTOs/WithoutId/SessionDTO.cs
@@ -0,0 +1,7 @@
+namespace DTOs.WithoutId;
+
+public class SessionDTO
+{
+    public string Email { get; set; }
+    public string Token { get; set; }
+}
