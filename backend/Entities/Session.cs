bdb65c84cf90252f15034410d9a1765310383100
AngheloZambrana <anghelozambrana2005@gmail.com>
2024-10-21 09:13:43 -0400

    fix: entities

--- backend/Entities/Session.cs
+++ backend/Entities/Session.cs
@@ -4,6 +4,7 @@ public class Session
 {
     public Guid SessionID { get; set; }
     public string Token { get; set; }
+    public DateTime CreationDate { get; set; }
 
     // Foreign keys
     public Guid ContactID { get; set; }
