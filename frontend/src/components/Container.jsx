e27b3f46f816826e8e9d4cb91e8d078764d007d7
MayerliSantander <mayerlisantander0@gmail.com>
2024-10-24 11:06:57 -0400

    add random room reservation

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Container({ children }) {
   return (
     <div className="flex flex-col items-center justify-center bg-white w-full">
-      <div className="bg-white border-2 border-rose-950 p-8 rounded-[20px] shadow-lg shadow-black/20 w-full max-w-xl
+      <div className="bg-white border-2 border-rose-950 p-8 rounded-[20px] shadow-lg shadow-black/20 w-full max-w-2xl
       ">
         {children}
       </div>
